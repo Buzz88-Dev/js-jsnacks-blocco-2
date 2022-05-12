df7542cbdda3adde0aedb014a0d1da27cec3eb04
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-12 14:43:08 +0200

    script ended

--- js/script1.js
+++ js/script1.js
@@ -1,3 +1,5 @@
+// genera un array di 10 numeri casuali range 1- 100, ma stavolta senza doppioni
+
 let myArray = [];
 console.log(myArray);
 
@@ -11,3 +13,46 @@ for (let i = 0; i < 10; i++){
 }
 
 console.log(myArray);
+
+
+
+
+// metodo alternativo
+let myArrayTwo = [];
+
+while (myArrayTwo.length < 10) {
+
+    let randomNum = Math.floor(Math.random() * 100) + 1;
+
+    if (!myArrayTwo.includes(randomNum)){
+        myArrayTwo.push(randomNum);
+    }
+}
+
+console.log(myArrayTwo);
+
+
+
+
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+
+let mioArray = [];
+console.log(mioArray);
+
+let somma = 0;
+
+while (somma <= 50){
+
+    let numero = parseInt(prompt("Dammi un numero da 1 a 10!!!"));
+    mioArray.push(numero);
+    somma += numero;
+    console.log(numero);
+}
+
+if (somma > 50) {
+    mioArray.pop();
+}
+
+console.log(mioArray);
+
